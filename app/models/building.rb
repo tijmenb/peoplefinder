b00bea92a891e2014dd488d5ca210601452978c0
Nuno Costa <nuno.mmc@gmail.com>
2015-11-16 23:29:43 +0000

    Remove `to_human` useless method adopting to use the concatenated_field helper

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,7 +1,3 @@
 class Building < ActiveRecord::Base
   scope :by_name, ->{ order :address }
-
-  def to_human
-    self.address
-  end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,3 @@
 class City < ActiveRecord::Base
   scope :by_name, ->{ order :name }
-
-  def to_human
-    self.name
-  end
 end
