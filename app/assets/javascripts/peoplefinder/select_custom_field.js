4f06816bb1ef51bb042150aecf751d07c3fb92ca
Nuno Costa <nuno.mmc@gmail.com>
2015-11-19 00:50:48 +0000

    Js auto selecting "Other..." in case custom_* has some value

--- app/assets/javascripts/peoplefinder/select_custom_field.js
+++ app/assets/javascripts/peoplefinder/select_custom_field.js
@@ -1,25 +1,45 @@
 $(function() {
   'use strict';
 
-  var customFieldListener = function(dropdown_id, hidden_field_id, option_selected) {
+  var showField = function(custom_field, hidden_field_id) {
+    custom_field.show();
+    var custom_field_input = custom_field.find(hidden_field_id);
+    custom_field_input.disabled = false;
+  };
+
+  var enableField = function(text_field) {
+    text_field.prop('disabled', false);
+  };
+
+  var customFieldListener = function(dropdown_id, option_selected) {
     option_selected = option_selected || 'Other...';
 
     $(dropdown_id).change(function(e) {
-
       if( $(this).find("option:selected").text() == option_selected ) {
-        var custom_field = $(this).closest('.form-group').find('.select_custom_field');
-        custom_field.show();
-        var custom_field_input = custom_field.find(hidden_field_id)[0];
-        custom_field_input.disabled = false;
+        var custom_field = $(this).closest('.form-group').find('.select_custom_field')
+        showField(custom_field);
+        enableField(custom_field.find('input[disabled=disabled]'));
       }
     });
   };
 
   var updateCustomFields = function() {
-    customFieldListener('select#person_building_id', '#person_custom_building');
-    customFieldListener('select#person_city_id', '#person_custom_city');
+    customFieldListener('select#person_building_id');
+    customFieldListener('select#person_city_id');
+  };
+
+  var selectOtherOption = function() {
+    $('.select_custom_field > input').each(function() {
+      if( $( this ).val() != '' ) {
+        enableField( $( this ) );
+        var parent = $( this ).closest('.form-group');
+        showField( parent.find('.select_custom_field') );
+        parent.find('select > option').last().prop('selected', true);
+      };
+    });
   };
 
   // Update on page load
   updateCustomFields();
+  selectOtherOption();
 });
