a092790e5b1a91539aaa1dfe9168e52fa613e9d7
Nuno Costa <nuno.mmc@gmail.com>
2015-11-23 14:11:57 +0000

    Hide edit-links div if there's no link visible

--- app/assets/javascripts/peoplefinder/help-area.js
+++ app/assets/javascripts/peoplefinder/help-area.js
@@ -4,3 +4,7 @@ $('.help-content').hide();
 $('.help-toggle').click(function(){
   $(this).toggleClass('open').next('.help-content').slideToggle('slow');
 });
+
+if( $('.edit-links > a').size() == 0 ){
+  $('.edit-links').hide();
+}
