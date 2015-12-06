51da5f4777ae9157df1b0479aecdc8eb1ff532af
Nuno Costa <nuno.mmc@gmail.com>
2015-12-06 21:36:35 +0000

    FIX#35 - accessing /search was returning 500 error

--- app/services/person_search.rb
+++ app/services/person_search.rb
@@ -1,16 +1,26 @@
 class PersonSearch
-  def fuzzy_search(query, max = 100)
+  def fuzzy_search(text, max = 100)
     Person.search(
       size: max,
-      query: {
-        fuzzy_like_this: {
-          fields: [
-            :name, :tags, :description, :location_in_building, :building_value,
-            :city_value, :role_and_group, :community_name, :staff_nr,
-          ],
-          like_text: query, prefix_length: 3, ignore_tf: true
-        }
-      }
+      query: search_query(text)
     ).records.limit(max)
   end
+
+  private
+    def search_query(text)
+      if text.present?
+        {
+          fuzzy_like_this: {
+            fields: [
+              :name, :tags, :description, :location_in_building, :building_value,
+              :city_value, :role_and_group, :community_name, :staff_nr,
+            ],
+            like_text: text, prefix_length: 3, ignore_tf: true
+          }
+        }
+      else
+        {match_all: {}}
+      end
+    end
+
 end
